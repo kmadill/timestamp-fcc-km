41ab4eda64a8027d43edaa4357e0b40d61e99230
Kevin Madill <kmadill@gmail.com>
2017-06-11 11:28:54 +0200

    third commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var path = require("path");
+var http = require("http");
 
 var app = express();
 
 app.get("/:parameter", function(req, res) {
   res.send("Hello, " + req.params.parameter)
 });
+
+http.createServer(app).listen(1337);
